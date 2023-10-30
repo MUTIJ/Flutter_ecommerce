7377765417281f18fe225b4b688bcf33f24bf872
Joseph Hezron Murithi <Jeihezmurithi@gmail.com>
2023-10-30 17:58:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
- 
+
+        
         scaffoldBackgroundColor: Colors.red,
         primarySwatch: Colors.blue,
       ),
